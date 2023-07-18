22be28c64808595dbbcca19a37dda16ca109c00f
Sumeyye <sumeyyesabirli@gmail.com>
2023-07-18 14:51:48 +0300

    migration oluşturuldu

--- CasgemDataAccessLayer/Concrete/Context.cs
+++ CasgemDataAccessLayer/Concrete/Context.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CasgemEntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,15 @@ using System.Threading.Tasks;
 
 namespace CasgemDataAccessLayer.Concrete
 {
-    public class Context
+    public class Context:DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=SUM;initial catalog=CasgemApi;integrated security=true");
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
     }
 }
